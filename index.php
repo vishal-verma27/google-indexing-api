d1e98575742664b4d7cf67f79d645b2d99c9ed85
Vishal Verma <vishalverma2701@gmail.com>
2022-11-12 16:13:04 +0530

    Changes Index Page

---
+++ index.php
@@ -0,0 +1,211 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="100% Free Online Google Indexing API Tool For WordPress, Blogger, or Any Websites: Instant Index Your Website Using Google Indexing API" />
+    <link rel="canonical" href="https://google-indexing-api-tool.pykit.org/" />
+    <meta property="og:title" content="100% Free Online Google Indexing API Tool For Any Website" />
+    <meta property="og:description" content="100% Free Online Google Indexing API Tool" />
+    <meta property="og:url" content="https://google-indexing-api-tool.pykit.org/" />
+    <meta property="og:site_name" content="Online Google Indexing API Toolkit" />
+    <meta property="og:locale" content="en_US" />
+    <meta property="og:type" content="website" />
+    <meta name="twitter:card" content="summary" />
+    <meta name="twitter:description" content="Use Our 100% Free Online Google Indexing API Tool For WordPress, Blogger, or Any Websites: Instant Index Your Website Using Google Indexing API" />
+    <meta name="twitter:title" content="100% Free Online Google Indexing API Tool For Any Website" />
+    <title>Online Google Indexing API - Toolkit For Instant Indexing</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" rel="stylesheet">
+    <link rel="shortcut icon" href="https://pykit.org/wp-content/uploads/2022/01/py-1.png" type="image/x-icon">
+    <link rel="stylesheet" href="./style/style.css">
+  </head>
+  <?php
+    define('gapi',TRUE);
+    // to send api request
+  ?>
+  <body class="bodyClass">
+  <!-- NAV BAR START-->
+  <nav class="navbar navbar-expand-lg bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="https://google-indexing-api-tool.pykit.org/">Online Google Indexing API Toolkit</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="https://google-indexing-api-tool.pykit.org/">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="https://pykit.org/contact-us/" target ="_blank">Contact Us</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Useful Site Links
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="https://google-indexing-api-tool.pykit.org/privacy-policy.html/" target ="_blank">Privacy Policy</a></li>
+            <li><a class="dropdown-item" href="https://google-indexing-api-tool.pykit.org/terms-and-conditions.html/" target ="_blank">Terms and Conditions</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="https://pykit.org/" target ="_blank">Pykit Blog</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+  <!-- NAV BAR END -->
+    <!-- Content Starts Here -->
+    <div class="content-description">
+    <h2 style="text-align: left;">What Is An API?</h2>
+<div>An API or Application Programming Interface is a connection between computers or computer programs. API helps two different computers to communicate and exchange data with each other.</div>
+<div><br /></div>
+<h2 style="text-align: left;">What Is Google Indexing API?</h2>
+<div>Google Indexing API or Indexing API is a service provided by Google to request for a fresh crawl of your web pages or URLs or for Instant Indexing of the web pages or URLs.</div>
+<div>The Indexing API allows any website owner to directly notify Google when pages are added or removed from the websites. This allows Google to schedule for a fresh crawl. This can lead to higher-quality traffic.</div>
+<div>Currently, the indexing API can only be used for Job Posting or Broadcast Event types web pages or URLs. But you can also request for websites/ blogs of other niches without any issue.</div>
+<div><br /></div>
+<h2 style="text-align: left;">What We Can Do With The Google Indexing API?</h2>
+<div>
+   <div style="text-align: left;">
+      <ol style="text-align: left;">
+         <li><b>Update a URL:</b> Notify Google of a new URL to crawl or that content at a previously-submitted URL has been updated.</li>
+         <li><b>Remove a URL:</b> After you delete a page from your servers, notify Google so that we can remove the page from our index and so that we don't attempt to crawl the URL again.</li>
+         <li><b>Get the status of a request:</b> Check the last time Google received each kind of notification for a given URL.</li>
+         <li><b>Send batch indexing requests:</b> Reduce the number of HTTP connections your client has to make by combining up to 100 calls into a single HTTP request.</li>
+      </ol>
+      <div><b>NOTE:</b> In the above Online Google Indexing Toolkit, we have options to Update or Remove URLs from Google Search Results. Only 20 URLs at a time.</div>
+      <div><br /></div>
+   </div>
+</div>
+<h2 style="text-align: left;">How To Get Google Indexing API Access Key Or Service Authorization Key?</h2>
+<div>To access the Indexing API, you need to verify that you are the owner of the website for which you are submitting requests to Google for Updating or Removing URLs from Google Search Results. To verify the ownership you have to obtain a Service Authorization Key/ API Access Key which is a JSON file. This is a one-time process and is completely free. The complete process is described in <a href="https://pykit.org/how-to-use-google-indexing-api-in-blogger-or-wordpress-in-2022/" target="_blank"><b>How To Obtain Service Authorization Key from Google</b></a>.</div>
+<div><br /></div>
+<h2 style="text-align: left;">How To Setup Google Search Console To Use Google Indexing API?</h2>
+<div>To use the Indexing API Service, you have to add the Service Account Email ID as Owner in your Google Search Console Property Setting. The Service Account Email ID can be found in the JSON file obtained in the previous step.</div>
+<div><br /></div>
+<div>Follow these steps to Add Service Account Email ID in your Google Search Console Property Setting:</div>
+<div>
+   <ol style="text-align: left;">
+      <li>Open Google Search Console &gt;&gt;&gt; Select Your Website In Property Drop-down</li>
+      <li>Scroll down and search for <b>Settings</b>&nbsp;in the bottom left corner and click on <b>Settings</b></li>
+      <li>In <b>General Settings</b>, click <b>Users and Permissions</b></li>
+      <li>Click on the <b>+Add User</b> button in the top right corner<br /></li>
+      <li>Enter Service Account Email ID and select '<b>Owner</b>' using the Permission drop-down option</li>
+      <li>Click <b>ADD</b> and it's done!</li>
+   </ol>
+</div>
+<h2 style="text-align: left;">How To Use Google Indexing API For WordPress/ Blogger/ Any Website URLs?</h2>
+<div>Steps to use the Free Online Google Indexing API Toolkit for WordPress/ Blogger/ Any Website:</div>
+<div>
+   <ol style="text-align: left;">
+      <li>Open Online Indexing API Toolkit</li>
+      <li>Fill in the user details- First Name Last Name, and Email ID</li>
+      <li>Provide the URLs list (only the first 20 URLs will be processed at a time)</li>
+      <li>Check the update type- Update URL (for fresh crawl request)/ Remove URL (remove URLs from the Google Search Results)</li>
+      <li>Upload JSON key file</li>
+      <li>Click on <b>Send Data</b> to submit the request. Check the <b>Response Summary</b> for Server Response.</li>
+      <li>Click on <b>Send Again</b> to submit a New Request</li>
+   </ol>
+</div>
+<div>
+   <h2>What Is Daily URL Request Quota Or Limit?</h2>
+</div>
+<div>
+   <ul style="text-align: left;">
+      <li><b>DefaultPublishRequestsPerDayPerProject:</b>&nbsp;The default daily per project quota for how many publish requests that you can send to the publish endpoint. This includes both <b>URL_UPDATED</b> and <b>URL_DELETED</b> request types. The default value is set to <b>200</b>.</li>
+      <li><b>DefaultMetadataRequestsPerMinutePerProject:</b>&nbsp;The default per minute per project quota for how many Read Only requests that you can send to the getMetadata endpoint. The default value is set to <b>180</b>.</li>
+      <li><b>DefaultRequestsPerMinutePerProject:</b>&nbsp;The default per minute per project quota for all endpoints. The default value is set to <b>600</b>.</li>
+   </ul>
+   <div>Check your available Quota using <a href="https://console.cloud.google.com/apis/api/indexing.googleapis.com/quotas" rel="nofollow" target="_blank">Google API Console</a>.</div>
+</div>
+    </div>
+    <!-- Content Ends Here -->
+<!-- Footer -->
+<footer class="text-center text-lg-start bg-light text-muted">
+  <!-- Section: Social media -->
+  
+  <!-- Section: Social media -->
+
+  <!-- Section: Links  -->
+  <section class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom"
+  style="background-color: rgba(0, 0, 0, 0);">
+    <div class="container text-center text-md-start mt-2">
+      <!-- Grid row -->
+      <div class="row mt-3">
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-2">
+          <!-- Content -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            <i class="text-reset"></i>About Pykit
+          </h6>
+          <p>
+          <a href="https://pykit.org" target ="_blank">Pykit</a> is a free technical blog.</p>
+        </p>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            Useful Site Links
+          </h6>
+          <p>
+            <a href="https://google-indexing-api-tool.pykit.org/privacy-policy.html/" target ="_blank" class="text-reset">Privacy Policy</a>
+          </p><br/>
+          <p>
+            <a href="https://google-indexing-api-tool.pykit.org/terms-and-conditions.html/" target ="_blank" class="text-reset">Terms and Conditions</a>
+          </p><br/>
+          <p>
+            <a href="https://pykit.org/" target ="_blank" class="text-reset">Pykit Blog</a>
+          </p>
+
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            Tools
+          </h6>
+          <p>
+            <a href="https://google-indexing-api-tool.pykit.org/" class="text-reset">Online Google Indexing API</a>
+          </p>
+
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">Contact</h6>
+          <p>
+            <a href="https://pykit.org/contact-us/" target ="_blank" class="text-reset">Contact Us</a>
+          </p><br/>
+          <p>
+            <i class="text-reset"></i>
+            Or Write Us @info@pykit.org
+          </p>
+        </div>
+        <!-- Grid column -->
+      </div>
+      <!-- Grid row -->
+    </div>                                                                               
+  </section>
+  <!-- Section: Links  -->
+
+  <!-- Copyright -->
+  <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
+    © 2022 Copyright:
+    <a class="text-reset fw-bold" href="https://pykit.org" target ="_blank">Pykit </a><span style="color: #e25555;">&#9829;</span>
+  </div>
+  <!-- Copyright -->
+</footer>
+<!-- Footer -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+  </body>
+</html>
